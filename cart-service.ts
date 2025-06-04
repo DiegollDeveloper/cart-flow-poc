601789ce5309cc864686293a654195a7dd3f0556
Diego Gonzalez <diego18_98@hotmail.com>
2025-06-03 22:21:39 -0500

    cart flow

---
+++ cart-service.ts
@@ -0,0 +1,28 @@
+export class CartSerice {
+    public valor: number;
+
+    constructor(valorInicial: number = 0) {
+        this.valor = valorInicial;
+    }
+
+    // Obtener el valor actual del contador
+    public obtenerValor(): number {
+        return this.valor;
+    }
+
+    // Aumentar el contador en 1 (o por una cantidad específica)
+    public aumentar(cantidad: number = 1): void {
+        this.valor += cantidad;
+    }
+
+    // Disminuir el contador en 1 (o por una cantidad específica)
+    public disminuir(cantidad: number = 1): void {
+        this.valor -= cantidad;
+    }
+
+    // Reiniciar el contador al valor inicial
+    public reiniciar(): void {
+        this.valor = 0;
+    }
+}
+
---
+++ index.ts
@@ -0,0 +1,13 @@
+// Import the functions you need from the SDKs you need
+export { CartSerice } from './cart-service'
+
+
+// export async function loginWithEmail(email: string, password: string): Promise<string> {
+//     try {
+//         const response = await signInWithEmailAndPassword(auth, email, password);
+//         return response.user.uid;
+//     } catch (error) {
+//         console.log(error);
+//         return 'Error';
+//     }
+// }
